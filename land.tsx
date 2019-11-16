3c82be70b6c6fce1a5d21fcd2c077dfe028490b3
tawan21222423 <42997302+tawan21222423@users.noreply.github.com>
2019-11-16 22:17:54 +0700

    tsx

---
+++ land.tsx
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="land" tilewidth="32" tileheight="32" tilecount="1600" columns="40">
+ <image source="b8bM4Wx.jpg" width="1280" height="1280"/>
+ <terraintypes>
+  <terrain name="dirt road" tile="1095"/>
+  <terrain name="water" tile="1303"/>
+ </terraintypes>
+ <tile id="69" terrain="1,,1,1"/>
+ <tile id="109" terrain="1,1,,1"/>
+ <tile id="627" terrain=",1,1,1"/>
+ <tile id="706" terrain=",1,,"/>
+ <tile id="708" terrain="1,1,1,"/>
+ <tile id="1005" terrain=",,,0"/>
+ <tile id="1006" terrain=",,0,0"/>
+ <tile id="1007" terrain=",,0,"/>
+ <tile id="1046" terrain="0,0,0,"/>
+ <tile id="1057" terrain="0,,0,"/>
+ <tile id="1067" terrain="1,1,1,1"/>
+ <tile id="1095" terrain="0,0,0,0"/>
+ <tile id="1175" terrain="0,0,,"/>
+ <tile id="1176" terrain="0,,,"/>
+ <tile id="1182" terrain=",,,1"/>
+ <tile id="1183" terrain=",,1,1"/>
+ <tile id="1186" terrain="1,,1,"/>
+ <tile id="1304" terrain="1,,,"/>
+ <tile id="1342" terrain=",1,,1"/>
+ <tile id="1366" terrain=",,1,"/>
+ <tile id="1424" terrain="1,1,,"/>
+ <tile id="1425" terrain="1,,,"/>
+ <tile id="1448" terrain=",0,,0"/>
+ <tile id="1488" terrain=",0,,"/>
+ <tile id="1489" terrain="0,0,,0"/>
+ <tile id="1490" terrain="0,,0,0"/>
+ <tile id="1534" terrain=",0,0,0"/>
+</tileset>
