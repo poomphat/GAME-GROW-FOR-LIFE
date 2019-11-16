cc4e68bd4647b076fc6a38ca713e7a6df88c99b1
poomphat wawongmoon <pumphat@hotmail.co.th>
2019-11-17 02:35:56 +0700

    add

---
+++ test/core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,105 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
+    Texture img;
+    TiledMap tiledMap;
+    OrthographicCamera camera;
+    TiledMapRenderer tiledMapRenderer;
+    SpriteBatch sb;
+    Texture texture;
+    Sprite sprite;
+    
+    @Override public void create () {
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,w,h);
+        camera.update();
+        tiledMap = new TmxMapLoader().load("map.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        Gdx.input.setInputProcessor(this);
+
+        sb = new SpriteBatch();
+        texture = new Texture(Gdx.files.internal("char.tmx"));
+        sprite = new Sprite(texture);
+    }
+
+    @Override public void render () {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+        sb.begin();
+        sprite.draw(sb);
+        sb.end();
+    }
+
+    @Override public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override public boolean keyUp(int keycode) {
+        if(keycode == Input.Keys.LEFT)
+            camera.translate(-32,0);
+        if(keycode == Input.Keys.RIGHT)
+            camera.translate(32,0);
+        if(keycode == Input.Keys.UP)
+            camera.translate(0,32);
+        if(keycode == Input.Keys.DOWN)
+            camera.translate(0,-32);
+        if(keycode == Input.Keys.NUM_1)
+            tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+        if(keycode == Input.Keys.NUM_2)
+            tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
+        return false;
+    }
+
+    @Override public boolean keyTyped(char character) {
+
+        return false;
+    }
+
+    @Override public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override public boolean scrolled(int amount) {
+        return false;
+    }
+}
+ 
