4e775c16c78984a17f424ff33cb4c6c7b8fb0e66
poomphat <42997298+poomphat@users.noreply.github.com>
2019-11-19 03:32:48 +0700

    add

--- test/core/assets/tree1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="tree1" tilewidth="48" tileheight="96" tilecount="2" columns="2">
- <image source="tree1.png" width="96" height="96"/>
-</tileset>
--- test/core/thee.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="thee" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="thee.png" width="512" height="512"/>
-</tileset>
