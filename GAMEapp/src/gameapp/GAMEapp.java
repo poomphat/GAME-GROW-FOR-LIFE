1a0f72d48d9f3ff3649c4eb47800f67b52b9966f
poomphat <42997298+poomphat@users.noreply.github.com>
2019-11-18 21:02:56 +0700

    add

--- GAMEapp/src/gameapp/GAMEapp.java
+++ GAMEapp/src/gameapp/GAMEapp.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -13,32 +13,37 @@ package gameapp;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author poomphatwawongmoon
- */
+import java.io.*;
+
+import sun.audio.*;
+
+
 public class GAMEapp extends JPanel implements KeyListener {
 
-    public int px = 400, py = 400, width = 16, height = 16, speedx=0, speedy=0;
-    public int mapwidth = 800, mapheight = 800;
-    public int camX;
-    public int camY;
+    private int px = 400, py = 400, width = 24, height = 24, speedx=0, speedy=0;
+    private int mapwidth = 800, mapheight = 800;
+    private int camX;
+    private int camY;
     private double zoom = 2.0;
-    private int movespeed = 2;
     int maxspeed = 5;
     int speed = 1;
+    int[] dx,dy;
+    int indexdin = 0;
     int acceleration = 1;
-    boolean movesUp;
-    Image currentImage;
+    boolean havedin=false;
+    din Din = new din();
+    Image currentImage = new ImageIcon("asset/char1.png").getImage();
+    Image point = new ImageIcon("asset/pointer.png").getImage();
     Image imgup = new ImageIcon("asset/up.png").getImage();
     Image imgdown = new ImageIcon("asset/char1.png").getImage();
     Image imgleft = new ImageIcon("asset/left.png").getImage();
     Image imgright = new ImageIcon("asset/right.png").getImage();
-
+    
+    public GAMEapp() {
+    }
+    
+    
     public static void main(String[] args) {
         GAMEapp p = new GAMEapp();
         JFrame fr = new JFrame();
@@ -58,19 +63,38 @@ public class GAMEapp extends JPanel implements KeyListener {
         Image img1 = new ImageIcon("asset/starmap.png").getImage();
         camX = (int) (mapwidth / 2) - (px);
         camY = (int) (mapheight / 2) - (py);
-        currentImage = imgdown;
+        
         g2D.scale(zoom, zoom);
         g.translate(camX - 100, camY - 200);
         g.drawImage(img1, 0, 0, null);
+        
+        if(havedin){
+            for(int i = 0; indexdin > i;i++){
+            g2D.drawImage(Din.getimage(), Din.getX(i), Din.getY(i), 16, 16, null);
+            }
+            indexdin += 1;
+        }
+        g.drawImage(point, this.px-(this.px%16)+16, (int)this.py-(this.py%16)+16,16, 16, null);
         g.drawImage(currentImage, this.px, this.py, width, height, null);
+        
         update();
     }
+
 
     public void keyTyped(KeyEvent ke) {
     }
 
     public void keyPressed(KeyEvent ke) {
         int key = ke.getKeyCode();
+        if(key == KeyEvent.VK_Z){
+            Din.setpositiondinX(indexdin,(int)this.px-(this.px%16)+16);
+            Din.setpositiondinY(indexdin,(int)this.py-(this.py%16)+16);
+            havedin = true;
+        }
+         if(key == KeyEvent.VK_X){
+            Din.removedin((int)this.px-(this.px%16)+16,(int)this.py-(this.py%16)+16);
+            havedin = true;
+        }
         if (key == KeyEvent.VK_LEFT) {
             speedx = -4;
             if(px <= 32 ){
@@ -79,6 +103,7 @@ public class GAMEapp extends JPanel implements KeyListener {
                 speedx = -4;
             }
             currentImage = imgleft;
+            repaint();
         }
         if (key == KeyEvent.VK_RIGHT) {
             speedx = 4;
@@ -88,6 +113,7 @@ public class GAMEapp extends JPanel implements KeyListener {
                 speedx = 4;
             }
             currentImage = imgright;
+            repaint();
         }
         if (key == KeyEvent.VK_UP) {
             speedy = -4;
@@ -97,6 +123,7 @@ public class GAMEapp extends JPanel implements KeyListener {
                speed = -4;
             }
             currentImage = imgup;
+            repaint();
         }
         if (key == KeyEvent.VK_DOWN) {
             speedy = 4;
@@ -106,8 +133,9 @@ public class GAMEapp extends JPanel implements KeyListener {
                speed = 4;
             }
             currentImage = imgdown;
+            repaint();
         }
-        repaint();
+        
 
     }
 
