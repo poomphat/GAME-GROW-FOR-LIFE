ef6286c665e2f695b6d03cfe7517c49c74091ab0
tawan21222423 <42997302+tawan21222423@users.noreply.github.com>
2019-11-18 13:05:41 +0700

    s

--- test/core/assets/Outdoors (Spring).tsx
+++ test/core/assets/Outdoors (Spring).tsx
@@ -1,48 +1,146 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.0" name="Outdoors (Spring)" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
  <image source="Outdoors (Spring).png" width="400" height="1264"/>
- <tile id="466">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="-0.0473093" y="0" width="15.9905" height="15.9432"/>
+ <terraintypes>
+  <terrain name="road" tile="226"/>
+  <terrain name="grass" tile="351"/>
+ </terraintypes>
+ <tile id="177" terrain="0,,0,0"/>
+ <tile id="178" terrain="0,0,0,"/>
+ <tile id="183">
+  <objectgroup draworder="index" id="4">
+   <object id="4" x="15.8182" y="2.54545">
+    <polygon points="0,0 -3.81818,0.272727 -4.36364,1.27273 -5.72727,1.63636 -6.36364,2.45455 -8,3.27273 -8.36364,4.09091 -9.36364,5 -10.9091,5.90909 -12,7.63636 -13.9091,9.27273 -14,12.6364 -13.9091,13.3636 0.272727,13.4545 0.363636,13.4545"/>
+   </object>
   </objectgroup>
+  <animation>
+   <frame tileid="183" duration="500"/>
+   <frame tileid="1182" duration="500"/>
+  </animation>
  </tile>
- <tile id="541">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="10.5" y="4.25">
-    <ellipse/>
-   </object>
-   <object id="2" x="15.625" y="5.125" width="0.5" height="0.625">
-    <ellipse/>
-   </object>
-  </objectgroup>
+ <tile id="184">
+  <animation>
+   <frame tileid="184" duration="500"/>
+   <frame tileid="1183" duration="500"/>
+  </animation>
  </tile>
- <tile id="542">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="8.875" y="3.125">
-    <ellipse/>
-   </object>
-  </objectgroup>
+ <tile id="185">
+  <animation>
+   <frame tileid="185" duration="500"/>
+   <frame tileid="1184" duration="500"/>
+  </animation>
  </tile>
- <tile id="543">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="9.5" y="2.75">
-    <ellipse/>
-   </object>
-  </objectgroup>
+ <tile id="200" terrain=",,,0"/>
+ <tile id="201" terrain=",,0,0"/>
+ <tile id="202" terrain=",0,0,0"/>
+ <tile id="203" terrain=",,0,"/>
+ <tile id="208">
+  <animation>
+   <frame tileid="208" duration="500"/>
+   <frame tileid="1207" duration="500"/>
+  </animation>
  </tile>
- <tile id="544">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="7" y="3" height="0.375">
-    <ellipse/>
-   </object>
-   <object id="2" x="8.375" y="2.625">
-    <ellipse/>
-   </object>
-  </objectgroup>
+ <tile id="210">
+  <animation>
+   <frame tileid="210" duration="500"/>
+   <frame tileid="1209" duration="500"/>
+  </animation>
  </tile>
- <tile id="545">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="6.76523" y="8.79953"/>
-  </objectgroup>
+ <tile id="211">
+  <animation>
+   <frame tileid="211" duration="500"/>
+   <frame tileid="1210" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="225" terrain=",0,,0"/>
+ <tile id="226" terrain="0,0,0,0"/>
+ <tile id="228" terrain="0,,0,"/>
+ <tile id="233">
+  <animation>
+   <frame tileid="233" duration="500"/>
+   <frame tileid="1232" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="235">
+  <animation>
+   <frame tileid="235" duration="500"/>
+   <frame tileid="1290" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="250" terrain=",0,,"/>
+ <tile id="251" terrain="0,0,,"/>
+ <tile id="252" terrain="0,0,,0"/>
+ <tile id="253" terrain="0,,,"/>
+ <tile id="258">
+  <animation>
+   <frame tileid="258" duration="500"/>
+   <frame tileid="1257" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="260">
+  <animation>
+   <frame tileid="260" duration="500"/>
+   <frame tileid="1315" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="325" terrain=",,,1"/>
+ <tile id="326" terrain=",,1,1"/>
+ <tile id="327" terrain=",,1,"/>
+ <tile id="328" terrain=",1,1,1"/>
+ <tile id="333">
+  <animation>
+   <frame tileid="333" duration="500"/>
+   <frame tileid="334" duration="500"/>
+   <frame tileid="335" duration="500"/>
+   <frame tileid="336" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="350" terrain=",1,,1"/>
+ <tile id="351" terrain="1,1,1,1">
+  <animation>
+   <frame tileid="637" duration="100"/>
+   <frame tileid="638" duration="100"/>
+   <frame tileid="639" duration="100"/>
+   <frame tileid="640" duration="100"/>
+   <frame tileid="641" duration="100"/>
+   <frame tileid="642" duration="100"/>
+   <frame tileid="643" duration="100"/>
+   <frame tileid="644" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="352" terrain="1,,1,"/>
+ <tile id="353" terrain="1,1,1,"/>
+ <tile id="375" terrain=",1,,"/>
+ <tile id="376" terrain="1,1,,"/>
+ <tile id="377" terrain="1,,,"/>
+ <tile id="378" terrain="1,,1,1"/>
+ <tile id="403" terrain="1,1,,1"/>
+ <tile id="637">
+  <animation>
+   <frame tileid="637" duration="100"/>
+   <frame tileid="638" duration="100"/>
+   <frame tileid="639" duration="100"/>
+   <frame tileid="640" duration="100"/>
+   <frame tileid="641" duration="100"/>
+   <frame tileid="642" duration="100"/>
+   <frame tileid="643" duration="100"/>
+   <frame tileid="644" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1293">
+  <animation>
+   <frame tileid="1293" duration="500"/>
+   <frame tileid="1294" duration="500"/>
+   <frame tileid="1295" duration="500"/>
+   <frame tileid="1296" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="1318">
+  <animation>
+   <frame tileid="1318" duration="500"/>
+   <frame tileid="1319" duration="500"/>
+   <frame tileid="1320" duration="500"/>
+   <frame tileid="1321" duration="500"/>
+  </animation>
  </tile>
 </tileset>
