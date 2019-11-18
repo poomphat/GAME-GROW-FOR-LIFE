281692404b984d5d09ca6c70605705d94e2dd103
poomphat <42997298+poomphat@users.noreply.github.com>
2019-11-19 00:57:16 +0700

    add

--- GAMEapp/src/gameapp/GAMEapp.java
+++ GAMEapp/src/gameapp/GAMEapp.java
@@ -18,6 +18,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 public class GAMEapp extends JPanel implements KeyListener {
 
     private int px = 800, py = 800, width = 24, height = 24, speedx = 0, speedy = 0;
@@ -25,10 +26,10 @@ public class GAMEapp extends JPanel implements KeyListener {
     private int camX;
     private int camY;
     private double zoom = 2.0;
-    int maxspeed = 5;
+    int maxspeed = 5,vet;
     int speed = 1, sleep = 0;
-    int[] dx, dy;
-    int indexdin = 0;
+    int[] dx, dy,carrot = new int[9999];
+    int indexdin = 0, indexdins = 0;
     int acceleration = 1;
     boolean havedin = false;
     int daycount;
@@ -43,6 +44,11 @@ public class GAMEapp extends JPanel implements KeyListener {
 
     public GAMEapp() {
     }
+    public Graphics renderFrame(Graphics g){
+
+        return g;
+
+    }   
 
     public static void main(String[] args) {
         GAMEapp p = new GAMEapp();
@@ -56,32 +62,43 @@ public class GAMEapp extends JPanel implements KeyListener {
 
     public void paintComponent(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
+        super.paintComponent(g);
+        renderFrame(g);
         g2D.setColor(Color.BLACK);
         g.fillRect(0, 0, 9999, 9999);
-
+        Font myFont = new Font ("Courier New", 1, 17);
         Image img1 = new ImageIcon("asset/Holetown.png").getImage();
         camX = (int) (mapwidth / 2) - (px);
         camY = (int) (mapheight / 2) - (py);
-
+        g.setFont (myFont);
         g2D.scale(zoom, zoom);
         g.translate(camX - 100, camY - 200);
         g.drawImage(img1, 0, 0, null);
 
         if (havedin) {
             for (int i = 0; indexdin > i; i++) {
-                g2D.drawImage(Din.getimage(), Din.getX(i), Din.getY(i), 16, 16, null);
+                g2D.drawImage(Din.getimage(carrot,i), Din.getX(i), Din.getY(i), 16, 16, null);
             }
-            indexdin += 1;
+            indexdins += 1;
         }
         g.drawImage(point, this.px - (this.px % 16) + 16, (int) this.py - (this.py % 16) + 16, 16, 16, null);
         g2D.drawImage(sleepimage, 33 * 16, 30 * 16, 16, 16, null);
         g.drawImage(currentImage, this.px, this.py, width, height, null);
         g.drawString("Day " + daycount, this.px + 200, this.py - 150);
         if (sleep == 1) {
-            g2D.fillRect(0, 0, 9999, 9999);
-
+            g.fillRect(0, 0, 9999, 9999);
+            Din.grow();
             sleep = 0;
-
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(GAMEapp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+        if (((px >=  512) && (px <= 528)) && ((py >= 448) && (py <= 484))) {
+            g.drawString("SLEEP HERE", this.px-50, this.py - 150);
         }
         update();
     }
@@ -95,22 +112,30 @@ public class GAMEapp extends JPanel implements KeyListener {
             if (((px >=  512) && (px <= 528)) && ((py >= 448) && (py <= 464))) {
                 daycount += 1;
                 sleep = 1;
-                
+     
                 try {
                     TimeUnit.SECONDS.sleep(1);
+                    
                 } catch (InterruptedException ex) {
                     Logger.getLogger(GAMEapp.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                daycount += 1;
-                repaint();
             }
-        } else {
-
+        } 
+        if (key == KeyEvent.VK_S) {
+            Din.setpositiondinX(indexdin, (int) this.px - (this.px % 16) + 16);
+            Din.setpositiondinY(indexdin, (int) this.py - (this.py % 16) + 16);
+            Din.setvet(indexdin);
+            havedin = true;
+            carrot[indexdin] = 1;
+            indexdin += 1;
         }
         if (key == KeyEvent.VK_Z) {
             Din.setpositiondinX(indexdin, (int) this.px - (this.px % 16) + 16);
             Din.setpositiondinY(indexdin, (int) this.py - (this.py % 16) + 16);
             havedin = true;
+            carrot[indexdin] = 0;
+            vet = 0;
+            indexdin += 1;
         }
         if (key == KeyEvent.VK_X) {
             Din.removedin((int) this.px - (this.px % 16) + 16, (int) this.py - (this.py % 16) + 16);
@@ -153,7 +178,7 @@ public class GAMEapp extends JPanel implements KeyListener {
             currentImage = imgdown;
             repaint();
         }
-
+        repaint();
     }
 
     public void update() {
--- GAMEapp/src/gameapp/din.java
+++ GAMEapp/src/gameapp/din.java
@@ -12,13 +12,36 @@ import javax.swing.*;
 public class din {
 
     Image din = new ImageIcon("asset/1b.png").getImage();
-    int[] dx = new int[9999], dy = new int[9999], ddx = new int[10000], ddy = new int[10000];
-    int remove;
+    Image carrot1 = new ImageIcon("asset/crop/carrot1.png").getImage();
+    Image carrot2 = new ImageIcon("asset/crop/carrot2.png").getImage();
+    Image carrot3 = new ImageIcon("asset/crop/carrot3.png").getImage();
+    Image current;
+    int[] dx = new int[9999], dy = new int[9999], ddx = new int[10000], ddy = new int[10000],vetstage = new int[10000],countday = new int[10000];
+    int remove,count;
 
-    public Image getimage() {
-        return din;
+    public Image getimage(int[] vet,int i) {
+        if(vet[i] == 0){
+            current = din;
+        }
+        if(vet[i] == 1 && vetstage[i] == 1){
+            current = carrot1;
+        }
+        if(vet[i] == 1 && vetstage[i] == 2){
+            current = carrot2;
+        }
+        if(vet[i] == 1 && vetstage[i] == 3){
+            current = carrot3;
+        }
+        return current;
     }
-
+    public void grow(){
+         for (int j = 0; vetstage.length-1 > j; j++) {
+             vetstage[j] += 1;
+         }
+    }
+    public void countday(int d){
+        count += 1;
+    }
     public int getX(int i) {
         return dx[i];
     }
@@ -34,6 +57,9 @@ public class din {
 
     public void setpositiondinY(int index, int posY) {
         this.dy[index] = posY;
+    }
+    public void setvet(int vet) {
+        this.vetstage[vet] = 1;
     }
 
     public void removedin(int posX, int posY) {
